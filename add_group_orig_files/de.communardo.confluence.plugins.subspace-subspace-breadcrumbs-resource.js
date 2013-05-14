09a6bfbd6a06aa2847de36d1952184d029755699
Chris Small <chsmall@indiana.edu>
2013-05-14 15:41:21 -0400

    Add Group page

---
+++ add_group_orig_files/de.communardo.confluence.plugins.subspace-subspace-breadcrumbs-resource.js
@@ -0,0 +1,15 @@
+AJS.toInit(	function() {
+	modifyConfluenceBreadCrums();
+});
+
+function modifyConfluenceBreadCrums() {
+	
+	if( typeof( window[ 'subspaceBreadCrumPath' ] ) != "undefined" && subspaceBreadCrumPath.length > 0) {
+		if(jQuery('#breadcrumbs').children()[1]) {
+			jQuery(jQuery('#breadcrumbs').children()[1]).remove();
+		}
+		for ( var pathCount = subspaceBreadCrumPath.length-1; pathCount >= 0; pathCount--) {
+			jQuery('#breadcrumbs').children(':first').after("<li><a href='"+contextPath+subspaceBreadCrumPath[pathCount][1]+"'>"+subspaceBreadCrumPath[pathCount][0]+'</a></li>');
+		}
+	}
+}
